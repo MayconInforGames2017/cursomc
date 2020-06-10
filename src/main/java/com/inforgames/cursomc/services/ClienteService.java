36f3704f3fb83fae1b731f27332c89d2a5717b48
Maycon R Santana <maycon_gus2009@hotmail.com>
2020-06-10 12:46:34 -0300

    Endoint / clientes/{id} disponivel

---
+++ src/main/java/com/inforgames/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.inforgames.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.inforgames.cursomc.domain.Cliente;
+import com.inforgames.cursomc.repositories.ClienteRepository;
+import com.inforgames.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id 
+				+ ", Tipo: " + Cliente.class.getName()));
+	}
+
+}
