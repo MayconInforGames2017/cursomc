d55eca92b3ae8b61b3e7ed9f7650cdcf86d9f6c4
Maycon R Santana <maycon_gus2009@hotmail.com>
2020-06-11 17:32:04 -0300

    Listando todas Categorias com DTO

--- src/main/java/com/inforgames/cursomc/services/CategoriaService.java
+++ src/main/java/com/inforgames/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.inforgames.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,18 +17,17 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria find(Integer id)  {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: " + id 
-					+ ", Tipo: " + Categoria.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
@@ -37,10 +37,13 @@ public class CategoriaService {
 		find(id);
 		try {
 			repo.deleteById(id);
-		}
-		catch (DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityViolationException("Não é possível excluir uma categoria que possui produtos");
 		}
 	}
-	
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
+
 }
